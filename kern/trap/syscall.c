b188943bca618108c4a19c77418384516262703c
mostafahakim03 <mostafahakim02@gmail.com>
2024-10-21 15:29:19 -0400

    Params Validation is Done

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -315,6 +315,13 @@ void sys_free_user_mem(uint32 virtual_address, uint32 size)
 void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
+	uint32 start =(uint32)USER_HEAP_START;
+	uint32 max =(uint32)USER_HEAP_MAX;
+	uint32* address=(uint32 *)virtual_address;
+	if(address==NULL||virtual_address==0)
+		env_exit();
+	if(virtual_address<start || (virtual_address+size) >max )
+		env_exit();
 
 	allocate_user_mem(cur_env, virtual_address, size);
 	return;
@@ -323,7 +330,13 @@ void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
-
+	uint32 start =(uint32)USER_HEAP_START;
+		uint32 max =(uint32)USER_HEAP_MAX;
+		uint32* address=(uint32 *)virtual_address;
+		if(address==NULL)
+			env_exit();
+		if(virtual_address<USER_HEAP_START || virtual_address >USER_HEAP_MAX )
+			env_exit();
 	allocate_chunk(cur_env->env_page_directory, virtual_address, size, perms);
 	return;
 }
