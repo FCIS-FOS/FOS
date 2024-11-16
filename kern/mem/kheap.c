f165698e5b89aaff0d56f848e04ec0d3ee95f718
Panda318 <mario4maged.mm@gmail.com>
2024-11-16 20:02:17 +0200

    add panic

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -15,6 +15,8 @@ int initialize_kheap_dynamic_allocator(uint32 daStart, uint32 initSizeToAllocate
 	//TODO: [PROJECT'24.MS2 - #01] [1] KERNEL HEAP - initialize_kheap_dynamic_allocator
 	// Write your code here, remove the panic and write your code
 	//panic("initialize_kheap_dynamic_allocator() is not implemented yet...!!");
+	if(daStart+initSizeToAllocate>daLimit)
+	 panic("exceeds limit");
     start=daStart;
 	brk=daStart+initSizeToAllocate;
 	limit=daLimit;
