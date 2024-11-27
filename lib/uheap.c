f096619c15256414b6caa6b8d30039fbb9dc9a5e
Mostafa-Farahat <mostafa.farahat78@gmail.com>
2024-11-27 20:23:25 +0200

    first iteration on smalloc

--- lib/uheap.c
+++ lib/uheap.c
@@ -102,8 +102,33 @@ void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
 	//==============================================================
 	//TODO: [PROJECT'24.MS2 - #18] [4] SHARED MEMORY [USER SIDE] - smalloc()
 	// Write your code here, remove the panic and write your code
-	panic("smalloc() is not implemented yet...!!");
-	return NULL;
+	// panic("smalloc() is not implemented yet...!!");
+	uint32 start_page_allocator=myEnv->uheap_limit+PAGE_SIZE;
+	uint32 num_of_pages=ROUNDUP(size,PAGE_SIZE)/PAGE_SIZE;
+	uint32 num_of_pages_unmarked=0;
+	uint32 start_virtual_addr=0;
+
+	for (uint32 addr = myEnv->uheap_limit+PAGE_SIZE; addr < USER_HEAP_MAX; addr+=PAGE_SIZE)
+	{
+		
+		if(sys_is_page_marked(addr)==0){
+			if(num_of_pages_unmarked==0)start_virtual_addr=addr;
+			num_of_pages_unmarked++;
+		}
+		else {
+			num_of_pages_unmarked=0;
+			start_virtual_addr=0;
+		}
+
+		if(num_of_pages_unmarked==num_of_pages)break;
+	}
+	if(num_of_pages_unmarked==num_of_pages){
+		sys_createSharedObject(sharedVarName, size, isWritable, start_virtual_addr);
+		return start_virtual_addr;
+	}else{
+		return NULL;
+	}
+
 }
 
 //========================================
