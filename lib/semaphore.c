b156bfdb3bf8846a12568aba66411124bd0ad6e6
mostafahakim03 <mostafahakim02@gmail.com>
2024-12-05 18:31:49 -0500

    Create and Get Semaphore Function is done not tested yet

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -1,25 +1,56 @@
 // User-level Semaphore
 
 #include "inc/lib.h"
-
+#include "sched_helpers.h"
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
-	//[PROJECT'24.MS3]
+	//TODO: [PROJECT'24.MS3 - #02] [2] USER-LEVEL SEMAPHORE - create_semaphore
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("create_semaphore is not implemented yet");
+	//panic("create_semaphore is not implemented yet");
+	struct semaphore semaphores;
+	if(value<0) 
+	{
+		semaphores.semdata=NULL;
+		return semaphores;
+	}
+	struct __semdata* priv_data=(struct __semdata*)smalloc(semaphoreName,sizeof(struct __semdata),1);
+	// if (priv_data==NULL)return ;// if(value<0) return; ??????????????????
+	if(priv_data==NULL) 
+	{
+		semaphores.semdata=NULL;
+		return semaphores;
+	}
+	priv_data->count=value;
+	priv_data->lock=0;
+	strcpy(&priv_data->name,semaphoreName);
+	
+	init_queue(&priv_data->queue);
+	
+	struct semaphore semaphores;/*= (struct semaphore*)smalloc(semaphoreName,sizeof(struct semaphore),1)*/
+	semaphores.semdata=priv_data;
+	return semaphores; 
 	//Your Code is Here...
 }
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
-	//[PROJECT'24.MS3]
+	//TODO: [PROJECT'24.MS3 - #03] [2] USER-LEVEL SEMAPHORE - get_semaphore
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("get_semaphore is not implemented yet");
+	// panic("get_semaphore is not implemented yet");
+	struct __semdata* priv_data=(struct __semdata*)sget(ownerEnvID,semaphoreName);
+	struct semaphore semaphores;
+	if(priv_data==NULL)
+	{
+		semaphores.semdata=NULL;
+		return semaphores;
+	}
+	semaphores.semdata=priv_data;
+	return semaphores;
 	//Your Code is Here...
 }
 
 void wait_semaphore(struct semaphore sem)
 {
-	//[PROJECT'24.MS3]
+	//TODO: [PROJECT'24.MS3 - #04] [2] USER-LEVEL SEMAPHORE - wait_semaphore
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
 	panic("wait_semaphore is not implemented yet");
 	//Your Code is Here...
@@ -27,7 +58,7 @@ void wait_semaphore(struct semaphore sem)
 
 void signal_semaphore(struct semaphore sem)
 {
-	//[PROJECT'24.MS3]
+	//TODO: [PROJECT'24.MS3 - #05] [2] USER-LEVEL SEMAPHORE - signal_semaphore
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
 	panic("signal_semaphore is not implemented yet");
 	//Your Code is Here...
